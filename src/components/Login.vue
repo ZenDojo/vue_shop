03630699aa4ac661a2209c7e21483551fdd34e53
ZenDojo <68330321+ZenDojo@users.noreply.github.com>
2021-03-23 23:53:21 -0700

    login

---
+++ src/components/Login.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="" />
+      </div>
+      <!-- 登录表单区 -->
+      <el-form
+        label-width="0px"
+        ref="loginFormRef"
+        :model="loginForm"
+        :rules="loginFormRules"
+        class="demo-ruleForm login_form"
+      >
+        <!-- username -->
+        <el-form-item prop="username">
+          <el-input
+            v-model="loginForm.username"
+            prefix-icon="el-icon-user"
+          ></el-input>
+        </el-form-item>
+        <!-- password -->
+        <el-form-item prop="password">
+          <el-input
+            type="password"
+            v-model="loginForm.password"
+            prefix-icon="el-icon-lock"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="name" class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loginForm: {
+        username: "admin",
+        password: "123456",
+      },
+      loginFormRules: {
+        username: [
+          { required: true, message: "请输入登录名称", trigger: "blur" },
+          { min: 3, max: 10, message: "长度在 3 到 5 个字符", trigger: "blur" },
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 3, max: 10, message: "长度在 3 到 5 个字符", trigger: "blur" },
+        ],
+      },
+    };
+  },
+  methods: {
+    resetLoginForm() {
+      this.$refs.loginFormRef.resetFields();
+    },
+    login() {
+      this.$refs.loginFormRef.validate(async (valid) => {
+        if (!valid) return;
+        const { data: res } = await this.$http.post("login", this.loginForm);
+        console.log(res);
+        if (res.meta.status != 200) return this.$message.error("登录失败");
+        this.$message.success("登录成功");
+        window.sessionStorage.setItem("token", res.data.token);
+        this.$router.push("/home");
+      });
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+}
+.avatar_box {
+  width: 130px;
+  height: 130px;
+  border: 1px solid #eee;
+  border-radius: 50%;
+  padding: 10px;
+  box-shadow: 0 0 10px #ddd;
+  margin: -70px auto 0;
+  background-color: #fff;
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background-color: #eee;
+  }
+}
+.login_form {
+  margin-top: 25px;
+  //   box-sizing: border-box;
+  //   width: 100%;
+  padding: 0 50px;
+}
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
